1bfd1b3e0c33301c85e8f582c1b2c8deb14efbd3
Safia Khatoon <114645192+Safiakhatoon767@users.noreply.github.com>
2024-11-06 16:54:19 +0530

    Create If_else.sh

---
+++ If_else.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+<< Disclaimer
+
+is patr k sabhi ghatnaye kalpanic  hai
+
+Disclaimer
+
+
+
+
+read -p "Enter Gabbar Dilouge : " gb
+
+echo ""
+
+read -p "Enter Thakur Dilouge : " th
+
+
+
+
+
+if [[ $th == "nahi" ]];
+then
+        echo "jai veeru ki entry aur bhasad"
+else
+        echo "chup ho jao" 
+fi
+
+echo "sholey khatam"  
