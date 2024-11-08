490dd4752631724cb763c47f43b6ddded26f5aca
Safia Khatoon <114645192+Safiakhatoon767@users.noreply.github.com>
2024-11-08 12:57:47 +0530

    Create my_backup.sh

---
+++ my_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+<<note
+ This scripts takes backup of any distination path given in argument
+./backup.sh /home/ubuntu/Scripts
+note
+
+
+
+timestamp="$(date +'%y-%m-%d_%H-%M-%S')"
+
+backup_dir="${timestamp}_backup.zip"
+
+zip -r $backup_dir $1
+
+echo "BACKUP_COMPLETE"
